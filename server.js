e2d4d2faceb1e18e2d10630feb442b1b8c584806
Vomo319 <omwangheakugbe@gmail.com>
2025-01-03 11:56:19 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,13 +9,10 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*", // In production, replace with your frontend URL
+    origin: "*",
     methods: ["GET", "POST"]
   }
 });
-
-const users = new Map();
-const rooms = new Map();
 
 app.get('/', (req, res) => {
   res.send('Chat App Backend is running!');
@@ -24,86 +21,17 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
   console.log('A user connected:', socket.id);
 
-  socket.on('join', ({ username, room }) => {
+  socket.on('join_room', ({ username, room }) => {
     socket.join(room);
-    users.set(socket.id, { id: socket.id, username, room });
-    
-    // Add user to room
-    if (!rooms.has(room)) {
-      rooms.set(room, new Set());
-    }
-    rooms.get(room).add(socket.id);
-
-    // Send system message
-    io.to(room).emit('receive_message', {
-      id: Date.now().toString(),
-      type: 'system',
-      message: `${username} has joined the room`,
-      username: 'System',
-      room,
-      timestamp: Date.now()
-    });
-
-    // Send updated user list
-    const roomUsers = Array.from(rooms.get(room))
-      .map(id => users.get(id))
-      .filter(Boolean);
-    io.to(room).emit('user_list', roomUsers);
+    console.log(`User ${username} joined room ${room}`);
   });
 
   socket.on('send_message', (data) => {
-    const user = users.get(socket.id);
-    if (user) {
-      io.to(user.room).emit('receive_message', {
-        ...data,
-        timestamp: Date.now()
-      });
-    }
-  });
-
-  socket.on('typing_start', () => {
-    const user = users.get(socket.id);
-    if (user) {
-      socket.to(user.room).emit('typing_start', { username: user.username });
-    }
-  });
-
-  socket.on('typing_end', () => {
-    const user = users.get(socket.id);
-    if (user) {
-      socket.to(user.room).emit('typing_end', { username: user.username });
-    }
+    console.log('Message received:', data);
+    io.to(data.room).emit('receive_message', data);
   });
 
   socket.on('disconnect', () => {
-    const user = users.get(socket.id);
-    if (user) {
-      const { username, room } = user;
-      users.delete(socket.id);
-      
-      if (rooms.has(room)) {
-        rooms.get(room).delete(socket.id);
-        if (rooms.get(room).size === 0) {
-          rooms.delete(room);
-        } else {
-          // Send system message
-          io.to(room).emit('receive_message', {
-            id: Date.now().toString(),
-            type: 'system',
-            message: `${username} has left the room`,
-            username: 'System',
-            room,
-            timestamp: Date.now()
-          });
-
-          // Send updated user list
-          const roomUsers = Array.from(rooms.get(room))
-            .map(id => users.get(id))
-            .filter(Boolean);
-          io.to(room).emit('user_list', roomUsers);
-        }
-      }
-    }
     console.log('A user disconnected:', socket.id);
   });
 });
@@ -113,4 +41,3 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
