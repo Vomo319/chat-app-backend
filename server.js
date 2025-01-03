52e2ad5c170d97d63f962ffefe3d464d10e87101
Vomo319 <omwangheakugbe@gmail.com>
2025-01-03 12:08:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ const io = new Server(server, {
   }
 });
 
+const users = new Map();
+const rooms = new Map();
+
 app.get('/', (req, res) => {
   res.send('Chat App Backend is running!');
 });
@@ -23,15 +26,34 @@ io.on('connection', (socket) => {
 
   socket.on('join_room', ({ username, room }) => {
     socket.join(room);
-    console.log(`User ${username} joined room ${room}`);
+    users.set(socket.id, { id: socket.id, username, room, isOnline: true });
+    
+    if (!rooms.has(room)) {
+      rooms.set(room, new Set());
+    }
+    rooms.get(room).add(socket.id);
+
+    io.to(room).emit('user_list', Array.from(rooms.get(room)).map(id => users.get(id)));
   });
 
   socket.on('send_message', (data) => {
-    console.log('Message received:', data);
     io.to(data.room).emit('receive_message', data);
   });
 
+  socket.on('edit_message', ({ messageId, newText, room }) => {
+    io.to(room).emit('message_edited', { messageId, newText });
+  });
+
   socket.on('disconnect', () => {
+    const user = users.get(socket.id);
+    if (user) {
+      const { room } = user;
+      users.delete(socket.id);
+      if (rooms.has(room)) {
+        rooms.get(room).delete(socket.id);
+        io.to(room).emit('user_list', Array.from(rooms.get(room)).map(id => users.get(id)));
+      }
+    }
     console.log('A user disconnected:', socket.id);
   });
 });
