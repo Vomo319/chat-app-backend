7513662b839a213196cb987cb5d53897bce09db3
Vomo319 <omwangheakugbe@gmail.com>
2025-01-03 10:52:06 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,45 +1,30 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const cors = require('cors');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-    cors: {
-        origin: '*',
-        methods: ['GET', 'POST']
-    }
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.send('Chat App Backend is running!');
 });
 
-// Middleware
-app.use(cors());
-
-const users = {};
-
-// Socket.IO connections
+// Socket.io logic
 io.on('connection', (socket) => {
-    console.log(`User connected: ${socket.id}`);
+  console.log('A user connected:', socket.id);
 
-    socket.on('set_username', ({ username }) => {
-        users[socket.id] = { username, status: 'online' };
-        io.emit('update_user_status', users);
-    });
+  socket.on('send_message', (data) => {
+    io.emit('receive_message', data);
+  });
 
-    socket.on('send_message', (data) => {
-        const timestamp = new Date().toLocaleTimeString();
-        io.emit('receive_message', { ...data, timestamp });
-    });
-
-    socket.on('disconnect', () => {
-        console.log(`User disconnected: ${socket.id}`);
-        delete users[socket.id];
-        io.emit('update_user_status', users);
-    });
+  socket.on('disconnect', () => {
+    console.log('A user disconnected:', socket.id);
+  });
 });
 
-// Server running
 const PORT = process.env.PORT || 3000;
+
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
