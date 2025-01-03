b11f27ed26e5699f1e2184227ea43834c4c72a8b
Vomo319 <omwangheakugbe@gmail.com>
2025-01-03 14:06:37 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
-const crypto = require('crypto');
 
 const app = express();
 app.use(cors());
@@ -20,10 +19,7 @@ const rooms = new Map();
 const messages = new Map();
 
 const CORRECT_PASSWORD = '1010';
-
-function hashPassword(password) {
-  return crypto.createHash('sha256').update(password).digest('hex');
-}
+const FIXED_GROUP = 'main-group';
 
 app.get('/', (req, res) => {
   res.send('Chat App Backend is running!');
@@ -33,13 +29,16 @@ io.on('connection', (socket) => {
   console.log('A user connected:', socket.id);
 
   socket.on('join_room', ({ username, room, password }) => {
+    console.log('Join room attempt:', { username, room });
+
     if (password !== CORRECT_PASSWORD) {
+      console.log('Invalid password attempt');
       socket.emit('join_error', 'Invalid password');
       return;
     }
 
     socket.join(room);
-    users.set(socket.id, { id: socket.id, username, room, isOnline: true });
+    users.set(socket.id, { id: socket.id, username, room, isOnline: true, isActive: true });
 
     if (!rooms.has(room)) {
       rooms.set(room, new Set());
@@ -54,37 +53,91 @@ io.on('connection', (socket) => {
     const roomMessages = messages.get(room) || [];
     socket.emit('message_history', roomMessages);
 
-    io.to(room).emit('user_list', Array.from(rooms.get(room)).map(id => users.get(id)));
+    const roomUsers = Array.from(rooms.get(room)).map(id => users.get(id)).filter(Boolean);
+    io.to(room).emit('user_list', roomUsers);
     
+    console.log('User joined successfully:', { username, room });
     socket.emit('join_success', { username });
   });
 
   socket.on('send_message', (data) => {
     const { room, id, message, username, timestamp, duration } = data;
-    const newMessage = { id, message, username, timestamp, duration, seenBy: [username] };
+    console.log('Received message:', data);
+
+    const newMessage = { id, message, username, timestamp, duration, seenBy:[username], reactions: {} };
     const roomMessages = messages.get(room) || [];
     roomMessages.push(newMessage);
     messages.set(room, roomMessages);
 
+    console.log('Broadcasting message to room:', room);
     io.to(room).emit('receive_message', newMessage);
   });
 
   socket.on('edit_message', ({ messageId, newText, room }) => {
+    console.log('Edit message request:', { messageId, newText, room });
     const roomMessages = messages.get(room) || [];
     const messageIndex = roomMessages.findIndex(msg => msg.id === messageId);
     if (messageIndex !== -1) {
       roomMessages[messageIndex].message = newText;
       roomMessages[messageIndex].isEdited = true;
+      console.log('Message edited, broadcasting to room:', room);
       io.to(room).emit('message_edited', { messageId, newText });
     }
   });
 
   socket.on('message_seen', ({ messageId, username, room }) => {
+    console.log('Message seen:', { messageId, username, room });
     const roomMessages = messages.get(room) || [];
     const message = roomMessages.find(msg => msg.id === messageId);
     if (message && !message.seenBy.includes(username)) {
       message.seenBy.push(username);
+      console.log('Updating seen status for message:', messageId);
       io.to(room).emit('update_seen', { messageId, seenBy: message.seenBy });
+    }
+  });
+
+  socket.on('delete_message', ({ messageId, room }) => {
+    console.log('Delete message request:', { messageId, room });
+    const roomMessages = messages.get(room) || [];
+    const updatedMessages = roomMessages.filter(msg => msg.id !== messageId);
+    messages.set(room, updatedMessages);
+    console.log('Message deleted, broadcasting to room:', room);
+    io.to(room).emit('message_deleted', { messageId });
+  });
+
+  socket.on('update_user_status', ({ isActive, room }) => {
+    console.log('User status update:', socket.id, isActive);
+    const user = users.get(socket.id);
+    if (user) {
+      user.isActive = isActive;
+      const roomUsers = Array.from(rooms.get(room)).map(id => users.get(id)).filter(Boolean);
+      io.to(room).emit('user_list', roomUsers);
+      io.to(room).emit('user_status_update', { userId: socket.id, isActive });
+    }
+  });
+
+  socket.on('typing_start', ({ username, room }) => {
+    console.log('User started typing:', username);
+    socket.to(room).emit('typing_start', { username });
+  });
+
+  socket.on('typing_end', ({ username, room }) => {
+    console.log('User stopped typing:', username);
+    socket.to(room).emit('typing_end', { username });
+  });
+
+  socket.on('add_reaction', ({ messageId, emoji, username, room }) => {
+    console.log('Add reaction:', { messageId, emoji, username, room });
+    const roomMessages = messages.get(room) || [];
+    const message = roomMessages.find(msg => msg.id === messageId);
+    if (message) {
+      if (!message.reactions[emoji]) {
+        message.reactions[emoji] = [];
+      }
+      if (!message.reactions[emoji].includes(username)) {
+        message.reactions[emoji].push(username);
+        io.to(room).emit('message_reaction', { messageId, emoji, username });
+      }
     }
   });
 
@@ -92,11 +145,14 @@ io.on('connection', (socket) => {
     const user = users.get(socket.id);
     if (user) {
       const { room } = user;
-      users.delete(socket.id);
+      user.isOnline = false;
+      user.isActive = false;
       const roomData = rooms.get(room);
       if (roomData) {
         roomData.delete(socket.id);
-        io.to(room).emit('user_list', Array.from(roomData).map(id => users.get(id)));
+        const roomUsers = Array.from(roomData).map(id => users.get(id)).filter(Boolean);
+        console.log('User disconnected, updating user list for room:', room);
+        io.to(room).emit('user_list', roomUsers);
       }
     }
     console.log('A user disconnected:', socket.id);
@@ -108,3 +164,4 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
