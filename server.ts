be1170cb230214f66c7243672e81b1c2b90cd36d
Vomo319 <omwangheakugbe@gmail.com>
2025-01-09 17:11:18 +0200

    Update server.ts

--- server.ts
+++ server.ts
@@ -9,7 +9,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*", // In production, replace with your frontend URL
+    origin: "funchat1.vercel.app", // In production, replace with your frontend URL
     methods: ["GET", "POST"]
   }
 });
